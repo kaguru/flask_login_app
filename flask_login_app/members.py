7159808456fef2188437178ba4f79550240430fb
jeff <gacherujeff@gmail.com>
2019-07-12 21:45:12 +0300

    -Remove unused session import

--- flask_login_app/members.py
+++ flask_login_app/members.py
@@ -2,11 +2,7 @@ from flask import (
     Blueprint, render_template
 )
 from flask_login import login_required
-from .db import get_engine, get_session
 
-
-db_engine = get_engine()
-db_session = get_session()
 
 bp = Blueprint('members', __name__, url_prefix='/members')
 
